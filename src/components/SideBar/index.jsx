2e9e0c5d198a3a32f2b10336210f733e33b41e1c
sajjad ahmad <sajjadahmad5578@gmail.com>
2025-07-09 09:34:48 +0500

    Add RangeSlider and Rating components to SideBar, enhance filters for availability, size, price, and rating

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -6,9 +6,14 @@ import Checkbox from '@mui/material/Checkbox';
 import { Collapse } from 'react-collapse';
 import Button from '@mui/material/Button';
 import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
+import RangeSlider from 'react-range-slider-input';
+import 'react-range-slider-input/dist/style.css';
+import Rating from '@mui/material/Rating';
 
 const SideBar = () => {
   const [isOpenCategoryFilter, setIsOpenCategoryFilter] = useState(true);
+  const [isOpenAvailFilter, setIsOpenAvailFilter] = useState(true);
+  const [isOpenSizeFilter, setIsOpenSizeFilter] = useState(true);
   return (
     <aside className='sidebar !py-5'>
         <div className="box">
@@ -31,6 +36,72 @@ const SideBar = () => {
             </div>
             </Collapse>
         </div>
+
+
+        <div className="box">
+            <h3 className='w-full !pr-2 !mb-3 text-[16px] font-[600] flex items-center'>Availability 
+              <Button  onClick={()=>setIsOpenAvailFilter(!isOpenAvailFilter)} className='!w-[30px] !h-[30px] !min-w-[30px] !ml-auto !rounded-full !text-black'>
+                {
+                  isOpenAvailFilter ? <FaAngleUp/> : <FaAngleDown/>
+                }
+              </Button></h3>
+            <Collapse isOpened={isOpenAvailFilter}>
+            <div className="scroll !px-4 relative -left-[13px]"> 
+             <FormControlLabel control={<Checkbox size="small"/>} className='w-full'  label="Available (17)" />
+             <FormControlLabel control={<Checkbox size="small"/>} className='w-full'  label="In Stock (10)" />
+             <FormControlLabel control={<Checkbox size="small"/>} className='w-full'  label="Not Available (17)" />
+            </div>
+            </Collapse>
+        </div>
+
+
+
+        <div className="box">
+            <h3 className='w-full !pr-2 !mb-3 text-[16px] font-[600] flex items-center'>Size 
+              <Button  onClick={()=>setIsOpenSizeFilter(!isOpenSizeFilter)} className='!w-[30px] !h-[30px] !min-w-[30px] !ml-auto !rounded-full !text-black'>
+                {
+                  isOpenSizeFilter ? <FaAngleUp/> : <FaAngleDown/>
+                }
+              </Button></h3>
+            <Collapse isOpened={isOpenSizeFilter}>
+            <div className="scroll !px-4 relative -left-[13px]"> 
+             <FormControlLabel control={<Checkbox size="small"/>} className='w-full'  label="Large (5)" />
+             <FormControlLabel control={<Checkbox size="small"/>} className='w-full'  label="Small (12)" />
+             <FormControlLabel control={<Checkbox size="small"/>} className='w-full'  label="Medium (19)" />
+             <FormControlLabel control={<Checkbox size="small"/>} className='w-full'  label="XL (16)" />
+             <FormControlLabel control={<Checkbox size="small"/>} className='w-full'  label="XXL (8)" />
+            </div>
+            </Collapse>
+        </div>
+
+
+        <div className="box !mt-4">
+            <h3 className='w-full !pr-5 !mb-3 text-[16px] font-[600] flex items-center'>Filter By Price </h3>
+            <RangeSlider/>
+            <div className="d-flex !pt-4 !pb-2 priceRange">
+              <span className='!text-[13px]'>
+                From: <strong className='text-dark'>RS:{100}</strong>
+              </span>
+              <span className='!ml-10 !text-[13px]'>
+                From: <strong className='text-dark'>RS:{5000}</strong>
+              </span>
+            </div>
+        </div>
+
+
+        <div className="box !mt-4">
+            <h3 className='w-full !pr-5 !mb-3 text-[16px] font-[600] flex items-center'>Filter By Rating</h3>
+
+            <div className="rating flex flex-col w-full cursor-pointer gap-1">
+            <Rating name="size-small" defaultValue={4} size="small" className='cursor-pointer'  readOnly />
+            <Rating name="size-small" defaultValue={5} size="small" className='cursor-pointer' readOnly />
+            <Rating name="size-small" defaultValue={2} size="small" className='cursor-pointer' readOnly />
+            <Rating name="size-small" defaultValue={4} size="small" className='cursor-pointer'  readOnly />
+            </div>
+        </div>
+
+
+        
     </aside>
   )
 }
